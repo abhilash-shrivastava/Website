93914eb9c1271e5372a0a281412f823569cd63fd
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-09-24 16:00:42 -0700

    changed the website heading

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ echo "deploying to aws"
 # log into aws everything under this is executed under the ubuntu user
 ssh ubuntu@ec2-52-88-156-243.us-west-2.compute.amazonaws.com <<ubuntu
     echo "SSH connection done"
+    cd ~/Website
+    git pull github master
 ubuntu
